4014ec65107680e35b70fe4a62691ce39ddc10d1
awangelo <anggg.gaio@gmail.com>
2025-01-15 19:41:15 -0300

    feat: logica para sortear amigo e reiniciar o jogo

--- app.js
+++ app.js
@@ -14,3 +14,32 @@ function adicionarAmigo() {
     inputNome.value = ""; // Limpa o input
     document.getElementById("listaAmigos").innerHTML += nome + "<br>";
 }
+
+function sortearAmigo() {
+    // Nao sortear se tiver menos de 2 amigos
+    if (nomeAmigos.length < 2) {
+        return;
+    }
+
+    let sorteado = nomeAmigos[Math.floor(Math.random() * nomeAmigos.length)];
+    nroSorteios++;
+
+    let resultadoElement = document.getElementById("resultado");
+    let sorteadoItem = document.createElement("li");
+    sorteadoItem.classList.add("sorteado");
+    sorteadoItem.textContent = `Sorteio ${nroSorteios}: ${sorteado}`;
+    resultadoElement.append(sorteadoItem);
+    
+    nomeAmigos = []; // Nao permitir mais sorteios com nomes repetidos
+
+    setTimeout(() => {
+        document.getElementById("listaAmigos").innerHTML = "";
+    }, 800);
+}
+
+function reiniciar() {
+    nomeAmigos = [];
+    nroSorteios = 0;
+    document.getElementById("listaAmigos").innerHTML = "";
+    document.getElementById("resultado").innerHTML = "";
+}
